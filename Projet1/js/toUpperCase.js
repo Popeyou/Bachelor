406f0bf3ed953786b6a591c92ea4f451ce3e998e
Pierro0705 <33931468+Pierro0705@users.noreply.github.com>
2019-10-31 17:26:47 +0100

    lol

--- Projet1/js/toUpperCase.js
+++ Projet1/js/toUpperCase.js
@@ -1,10 +1,16 @@
-function normaliser ()
+function normaliserNom ()
 {   
     var chaine = document.getElementById('nom').value;
     var reg = "^[a-zA-Z-éè]{0,100}$";
     if (chaine.match(reg))
     {
-        
+        chaine = chaine.toUpperCase();
+
+        document.getElementById('nom').value = chaine;
+
+        document.getElementById('nom').style.borderWidth = "3px";
+        document.getElementById('nom').style.borderColor = "green";
+        document.getElementById('nom').style.color = "green";
     }
     else
     {
@@ -13,8 +19,39 @@ function normaliser ()
         document.getElementById('nom').style.color = "red";
     }
 
-    chaine = chaine.toUpperCase();
+    if (chaine == "")
+    {
+        document.getElementById('nom').style.borderWidth = "3px";
+        document.getElementById('nom').style.borderColor = "red";
+        document.getElementById('nom').style.color = "red";
+    }
+}
 
+function normaliserPrenom ()
+{   
+    var pre = document.getElementById('prenom').value;
+    var regex = "^[a-zA-Z-éè]{0,100}$";
+    if (pre.match(regex))
+    {
+        pre = pre.charAt(0).toUpperCase() + pre.substring(1).toLowerCase(); 
 
-    document.getElementById('nom').value = chaine;
+        document.getElementById('prenom').value = pre;
+
+        document.getElementById('prenom').style.borderWidth = "3px";
+        document.getElementById('prenom').style.borderColor = "green";
+        document.getElementById('prenom').style.color = "green";
+    }
+    else
+    {
+        document.getElementById('prenom').style.borderWidth = "3px";
+        document.getElementById('prenom').style.borderColor = "red";
+        document.getElementById('prenom').style.color = "red";
+    }
+
+    if (pre == "")
+    {
+        document.getElementById('prenom').style.borderWidth = "3px";
+        document.getElementById('prenom').style.borderColor = "red";
+        document.getElementById('prenom').style.color = "red";
+    }
 }
