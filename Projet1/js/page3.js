9131f4d9b5dd0768558eca36fd905ca0c32987a0
Jeremy D <33931501+Popeyou@users.noreply.github.com>
2019-11-02 18:18:45 +0100

    000

--- Projet1/js/page3.js
+++ Projet1/js/page3.js
@@ -3,7 +3,7 @@ var dateVerifie = 0;
 function verifDate() //verif de la date dans le bon format
 {
     var chaine = document.getElementById('datea').value; //date en variable
-    var regdate = "^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$"; //regex de la date
+    var regdate = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g; //regex de la date
 
     var a = document.getElementById("err");
 
@@ -45,3 +45,94 @@ function verifDate() //verif de la date dans le bon format
     }
 }
 
+
+function verifNbj()
+{
+    var chaine = document.getElementById('nbj').value; //date en variable
+    var regdate = /^[0-9]*$/g ; //regex de la date
+
+    var a = document.getElementById("err2");
+
+    if (chaine.match(regdate) && chaine < 31 ) //Si il n'y a que jj/mm/aaaa
+    {
+        document.getElementById('nbj').value = chaine;
+
+        document.getElementById('nbj').style.borderWidth = "3px"; //On augmente la taille du bord du champs html
+        document.getElementById('nbj').style.borderColor = "green"; //On change la couleur du champs en vert pour dire que tout va bien
+        document.getElementById('nbj').style.color = "green"; //Pareil pour la police
+
+        document.getElementById("err2").style.color = "green";
+        a.innerHTML = "Le format est valide &nbsp; <i class='fas fa-check'></i>";
+
+        dateVerifie = 1; //variable globale
+    }
+    else //sinon
+    {
+        document.getElementById('nbj').style.borderWidth = "3px"; //On augmente la taille du bord du champs html
+        document.getElementById('nbj').style.borderColor = "red"; //On change la couleur du champs en rouge pour indiquer que le format n'est pas le bon
+        document.getElementById('nbj').style.color = "red"; //Pareil pour la police
+
+        document.getElementById("err2").style.color = "red";
+        a.innerHTML = "Le format est invalide &nbsp; <i class='fas fa-times'></i>";
+
+        nomVerifie = 0; //variable globale
+    }
+
+    if (chaine == "") //Si le champs est vide
+    {
+        document.getElementById('nbj').style.borderWidth = "3px"; //Meme chose que le else
+        document.getElementById('nbj').style.borderColor = "red";
+        document.getElementById('v').style.color = "red";
+
+        document.getElementById("err2").style.color = "red";
+        a.innerHTML = "Insérez un nombre &nbsp; <i class='fas fa-times'></i>";
+
+        nomVerifie = 0; //variable globale
+    }
+}
+
+
+function verifUrl()
+{
+    var chaine = document.getElementById('url').value; //date en variable
+    var regdate = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g ; //regex de la date
+
+    var a = document.getElementById("err3");
+
+    if (chaine.match(regdate)) //Si il n'y a que jj/mm/aaaa
+    {
+        document.getElementById('url').value = chaine;
+
+        document.getElementById('url').style.borderWidth = "3px"; //On augmente la taille du bord du champs html
+        document.getElementById('url').style.borderColor = "green"; //On change la couleur du champs en vert pour dire que tout va bien
+        document.getElementById('url').style.color = "green"; //Pareil pour la police
+
+        document.getElementById("err3").style.color = "green";
+        a.innerHTML = "Le format est valide &nbsp; <i class='fas fa-check'></i>";
+
+        dateVerifie = 1; //variable globale
+    }
+    else //sinon
+    {
+        document.getElementById('url').style.borderWidth = "3px"; //On augmente la taille du bord du champs html
+        document.getElementById('url').style.borderColor = "red"; //On change la couleur du champs en rouge pour indiquer que le format n'est pas le bon
+        document.getElementById('url').style.color = "red"; //Pareil pour la police
+
+        document.getElementById("err3").style.color = "red";
+        a.innerHTML = "Le format est invalide &nbsp; <i class='fas fa-times'></i>";
+
+        nomVerifie = 0; //variable globale
+    }
+
+    if (chaine == "") //Si le champs est vide
+    {
+        document.getElementById('url').style.borderWidth = "3px"; //Meme chose que le else
+        document.getElementById('url').style.borderColor = "red";
+        document.getElementById('url').style.color = "red";
+
+        document.getElementById("err3").style.color = "red";
+        a.innerHTML = "Le format est invalide &nbsp; <i class='fas fa-times'></i>";
+
+        nomVerifie = 0; //variable globale
+    }
+}
