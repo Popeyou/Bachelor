265b61d5d42065d2db7aa7d5adac4e3b50ad2262
Pierro0705 <33931468+Pierro0705@users.noreply.github.com>
2019-11-05 18:05:37 +0100

    debout

--- Projet1/js/page4.js
+++ Projet1/js/page4.js
@@ -1,26 +1,33 @@
-function txt255() //Fonction de vérification de date en utilisant un regex
+function txt() //Fonction de vérification de date en utilisant un regex
 {
     var txt = document.getElementById('txt255').value;
     var d = document.getElementById("messageErr6");
     var nbtxt = txt.length;
+    document.getElementById('txt255').maxLength = 255;
 
     if (nbtxt < 255) 
     {
-        document.getElementById('dateNaiss').style.borderWidth = "3px";
-        document.getElementById('dateNaiss').style.borderColor = "green";
-        document.getElementById('dateNaiss').style.color = "green";
+        document.getElementById('txt255').style.borderWidth = "3px";
+        document.getElementById('txt255').style.borderColor = "green";
+        document.getElementById('txt255').style.color = "green";
 
         document.getElementById("messageErr6").style.color = "green";
         d.innerHTML = "Le format est valide &nbsp; <i class='fas fa-check'></i>";
+        document.getElementById('btn1').disabled = false;
     }
     else 
     {
-        document.getElementById('dateNaiss').style.borderWidth = "3px";
-        document.getElementById('dateNaiss').style.borderColor = "red";
-        document.getElementById('dateNaiss').style.color = "red";
+        document.getElementById('btn1').disabled = true;
+        document.getElementById('txt255').style.borderWidth = "3px";
+        document.getElementById('txt255').style.borderColor = "red";
+        document.getElementById('txt255').style.color = "red";
 
         document.getElementById("messageErr6").style.color = "red";
-        d.innerHTML = "Le format est invalide &nbsp; <i class='fas fa-times'></i>";
-
+        d.innerHTML = "Vous devez entrer moins de 255 caractères &nbsp; <i class='fas fa-times'></i>";
     }
+}
+
+function pageSuivante ()
+{
+    document.location.href="fin.html";
 }
